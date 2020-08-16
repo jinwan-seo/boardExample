cc221c09908565dd62e96d957c2ceba2acc0b0aa
jinwanseo <359627@naver.com>
2020-08-16 19:02:15 +0900

    third commit - create,modify,mysqlconnect,mainpage

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
 const http = require("http");
 const url = require("url");
+const create = require("./lib/create");
 
-const app = http
-  .createServer((request, response) => {
-    let queryData = url.parse(request.url, true).query;
-    let pathName = url.parse(request.url, true).pathname;
+const app = http.createServer((request, response) => {
+  let queryData = url.parse(request.url, true).query;
+  let pathName = url.parse(request.url, true).pathname;
 
-    if (pathName === "/") {
-      let template = `
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>NodeJs Test</title>
-          </head>
-          <body>
-            <section>
-              <h1>NodeJs</h1>
-              <ul>
-                <li>Html</li>
-                <li>Css</li>
-                <li>Script</li>
-              </ul>
-              <div id="btnArea">
-                <a href="/create">Create</a>
-              </div>
-              <div id="contentArea">
-                nodeJs is ... 
-              </div>
-            </section>
-          </body>
-        </html>        
-        `;
-
+  if (pathName === "/") {
+    create.html(queryData.id).then((x) => {
       response.writeHead(200);
-      response.end(template);
-    }
-  })
-  .listen(3000);
+      response.end(x);
+    });
+  } else if (pathName === "/create") {
+    create.form().then((formStr) => {
+      create.html(undefined, "create", formStr).then((page) => {
+        response.writeHead(200);
+        response.end(page);
+      });
+    });
+  } else if (pathName === "/create_process") {
+    create.upload(request, response);
+  } else if (pathName === "/modify") {
+    create.form(queryData.id).then((formStr) => {
+      create.html(undefined, "modify", formStr).then((page) => {
+        response.writeHead(200);
+        response.end(page);
+      });
+    });
+  } else if (pathName === "/modify_process") {
+    create.modify(request, response);
+  } else if (pathName === "/delete") {
+  } else {
+    response.writeHead(404);
+    response.end("Not Found");
+  }
+});
+app.listen(3000);
